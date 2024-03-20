38a2fb82850386a7ea887e197ed21179f12523ab
13101989 <valentin.damache@outlook.com>
2024-03-20 13:02:01 +0200

    fixed error in key

--- aws-ec2-wordpress-modules/main.tf
+++ aws-ec2-wordpress-modules/main.tf
@@ -14,14 +14,14 @@ module "key_pair" {
 module "ec2_instance" {
   source = "./modules/ec2-instance"
 
-  instance_type      = var.instance_type
-  ami_id             = var.ami_id
-  key_name           = module.key_pair.key_name
-  security_groups    = [module.security_group.security_group_name]
+  instance_type   = var.instance_type
+  ami_id          = var.ami_id
+  key_name        = module.key_pair.key_name
+  security_groups = [module.security_group.security_group_name]
 }
 
 
 output "private_key" {
-  value = module.key_pair.private_key
+  value     = module.key_pair.private_key
   sensitive = true
 }
--- aws-ec2-wordpress-modules/modules/key-pair/main.tf
+++ aws-ec2-wordpress-modules/modules/key-pair/main.tf
@@ -5,6 +5,6 @@ resource "tls_private_key" "private_key" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
-  public_key = tls_private_key.access_key.public_key_openssh
+  public_key = tls_private_key.private_key.public_key_openssh
 }
 
